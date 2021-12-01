7ccaa4adf1e2d9e16cfd241a570c560f98e3e3eb
Ramadan Velisha <ramadan.velisha@gmail.com>
2021-12-02 01:56:04 +0200

    Delete project directory

--- project/project/src/main/java/com/pizzafactory/project/repositories/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pizzafactory.project.repositories;
-
-import com.pizzafactory.project.entities.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
