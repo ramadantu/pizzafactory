ba4b1ec427376e9f0316074703203cd13c1a4e9e
Ramadan Velisha <rvelisha@tu-sofia.bg>
2021-12-02 01:45:15 +0200

    Delete project directory

--- project/project/src/main/java/com/pizzafactory/project/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pizzafactory.project.repositories;
-
-import com.pizzafactory.project.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
