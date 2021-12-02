af777c01f8cd6d33a0fd77920582ec1aaf67d8ac
Ramadan Velisha <rvelisha@tu-sofia.bg>
2021-12-02 02:00:09 +0200

    Add files via upload

---
+++ project/src/main/java/com/pizzafactory/project/repositories/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.pizzafactory.project.repositories;
+
+import com.pizzafactory.project.entities.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
