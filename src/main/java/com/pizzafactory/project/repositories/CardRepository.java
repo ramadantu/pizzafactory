36c3a069b3946164c2e21097037bf051b713505a
ramadantu <rvelisha@tu-sofia.bg>
2022-06-01 14:31:08 +0300

    Delete the old files

--- src/main/java/com/pizzafactory/project/repositories/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pizzafactory.project.repositories;
-
-import com.pizzafactory.project.entities.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-
-    Card findCardByClientEmail(String email);
-}
