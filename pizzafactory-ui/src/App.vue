bfdb5cf6412a885d90bad8b0b43986c0a31a1874
ramadantu <rvelisha@tu-sofia.bg>
2022-06-14 18:28:31 +0300

    Change title and icon of website

--- pizzafactory-ui/src/App.vue
+++ pizzafactory-ui/src/App.vue
@@ -13,7 +13,6 @@
           <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item to="/menu">Menu</b-nav-item>
           <b-nav-item to="/order">Order</b-nav-item>
-          <b-nav-item to="/about">About</b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
--- pizzafactory-ui/src/router/index.js
+++ pizzafactory-ui/src/router/index.js
@@ -21,14 +21,6 @@ const routes = [
     path: '/order',
     name: 'Order',
     component: OrderView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
